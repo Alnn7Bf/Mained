e45bea028b031b5894321d6d012d6cfd6ae973ad
Alnn7Bf <alanubf2018@gmail.com>
2025-12-11 03:27:38 -0600

    Mejora form

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -133,7 +133,7 @@ export const LinkedIn = ({size}) => {
 
 export const Check = ({size}) => {
     return  <>
-        <svg width={size} height={size} fill="none" stroke="#3f3" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg width={size} height={size} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M7 12.5L10 15.5L17 8.5"></path>
             <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"></path>
         </svg>
